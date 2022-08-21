b3357c662de743cb6cc9d002e71d382310c11294
Sahil Kumar <sahilkumar142002@gmail.com>
2022-08-22 01:44:49 +0530

    uploaded qb-clothing

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Clothing'
+
+ui_page 'html/index.html'
+
+shared_script 'config.lua'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/ComboZone.lua',
+	'client/main.lua'
+}
+files {
+	'html/index.html',
+	'html/style.css',
+	'html/reset.css',
+	'html/script.js',
+	'html/image.png',
+}
+
+lua54 'yes'
